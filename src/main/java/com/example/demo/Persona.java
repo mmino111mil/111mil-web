7119d0b393a727c8a6b7ccf58548f90a3aaf89fa
mmino111mil <mmino.111mil@gmail.com>
2019-03-25 23:14:30 -0300

    proyecto spring boot init

---
+++ src/main/java/com/example/demo/Persona.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Persona {
+     private  long id;
+    private  String content;
+
+    public Persona() {
+    }
+
+    public Persona(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+    
+    
+}
